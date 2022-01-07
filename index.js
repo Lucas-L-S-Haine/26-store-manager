ea864afebb271ac34f9397117193db22a784b400
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-07 15:28:04 -0300

    Início da aplicação.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(port, () => console.log(`Application online on port ${port}.`));

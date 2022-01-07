1d28c8b3cd03b0ea9efad09494fb4f242b078175
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-07 16:04:21 -0300

    Usa o `dotenv` e usa as rotas.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const router = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,4 +10,5 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use(router);
 app.listen(port, () => console.log(`Application online on port ${port}.`));

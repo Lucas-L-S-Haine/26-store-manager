fd2fe0a8fc762f48eec0412f96ced4164d26284e
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-12 12:58:38 -0300

    Erro no tratamento de erros.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,10 +4,9 @@ const insert = async (req, res, _next) => {
   try {
     const product = req.body;
     const response = await productsServices.newProduct(product);
-    console.log(response.ops[0]);
     return res.status(201).json(response.ops[0]);
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
     // next(err);
     return res.status(422).send(err.message);
   }
--- services/productsServices.js
+++ services/productsServices.js
@@ -2,11 +2,18 @@
 const productsModel = require('../models/productsModel');
 
 const newProduct = async (product) => {
-//   const { name, quantity } = product;
+  const { name, quantity } = product;
 //   const { error } = Joi.object({
 //     name: Joi.string().min(5).message({ 'string.min': 'xablau' }),
 //   }).validate({ name });
 //   if ( error ) {}
+  if (name.length < 5) {
+    const err = new Error({
+      code: 'invalid_data',
+      message: '"name" length must be at least 5 characters long',
+    });
+    throw err;
+  }
   const createdProduct = await productsModel.insertProduct(product);
   return createdProduct;
 };

4dcac26254d30f6dd78ce66acc16c74d899700d7
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-15 19:39:10 -0300

    Requisito 08 concluído.🚀

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,14 @@ const handleError = (err, req, res, _next) => {
     return res.status(err.status)
       .json({ err: { code: 'invalid_data', message: err.message } });
   }
-  return res.status(500).json({ message: 'Internal Server Error' });
+  const error = {
+    err: {
+      code: 'not_found',
+      message: 'Sale not found',
+      status: 404,
+    },
+  };
+  return res.status(error.err.status).json(error);
 };
 
 module.exports = handleError;
